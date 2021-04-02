414d6eab8c56876fffbd1219f2d63db57dd46b9c
Takahiro Yoneki <hayabusa186882@gmail.com>
2021-04-03 01:55:02 +0900

    CSRF対策

--- www/html/signup_process.php
+++ www/html/signup_process.php
@@ -5,27 +5,30 @@ require_once MODEL_PATH . 'user.php';
 
 session_start();
 
-if(is_logined() === true){
+if (is_logined() === true) {
   redirect_to(HOME_URL);
 }
 
 $name = get_post('name');
 $password = get_post('password');
 $password_confirmation = get_post('password_confirmation');
-
+$token = get_post('token');
 $db = get_db_connect();
 
-try{
-  $result = regist_user($db, $name, $password, $password_confirmation);
-  if( $result=== false){
+if (is_valid_csrf_token($token)) {
+  try {
+    $result = regist_user($db, $name, $password, $password_confirmation);
+    if ($result === false) {
+      set_error('ユーザー登録に失敗しました。');
+      redirect_to(SIGNUP_URL);
+    }
+  } catch (PDOException $e) {
     set_error('ユーザー登録に失敗しました。');
     redirect_to(SIGNUP_URL);
   }
-}catch(PDOException $e){
-  set_error('ユーザー登録に失敗しました。');
-  redirect_to(SIGNUP_URL);
+  set_message('ユーザー登録が完了しました。');
+} else {
+  set_error('不正な操作が行われました');
 }
-
-set_message('ユーザー登録が完了しました。');
 login_as($db, $name, $password);
 redirect_to(HOME_URL);
