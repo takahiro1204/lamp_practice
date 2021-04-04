52fb41abb73ed815d1c0685f54674f086a6ce268
Takahiro Yoneki <hayabusa186882@gmail.com>
2021-04-04 18:51:59 +0900

    テーブル設計

---
+++ lamp_dock/append_schema.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: mysql
+-- 生成日時: 2021 年 4 月 04 日 09:40
+-- サーバのバージョン： 5.7.33
+-- PHP のバージョン: 7.4.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `sample`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `details`
+--
+
+CREATE TABLE `details` (
+  `order_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `amount` varchar(100) NOT NULL,
+  `create_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `history`
+--
+
+CREATE TABLE `history` (
+  `order_id` int(11) NOT NULL,
+  `user_name` int(11) NOT NULL,
+  `create_datetime` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `history`
+--
+ALTER TABLE `history`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
