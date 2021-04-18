4d5683027628446766ba4c34e606ba4c75888ae6
Takahiro Yoneki <hayabusa186882@gmail.com>
2021-04-18 19:34:07 +0900

    追加画面の作成

--- www/view/detail_view.php
+++ www/view/detail_view.php
@@ -1,36 +1,62 @@
 <!DOCTYPE HTML>
 <html lang="ja">
-    <head>
-        <?php include VIEW_PATH . 'template/head.php'; ?>
-        <title>明細履歴</title>
-        <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . 'cart.css'); ?>">
-    </head>
-    <body>
-    <?php include VIEW_PATH.'template/cart.php'; ?>
+
+<head>
+    <?php include VIEW_PATH . 'template/head.php'; ?>
+    <title>明細履歴</title>
+    <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . 'detail.css'); ?>">
+</head>
+
+<body>
+    <?php include VIEW_PATH . 'templates/header_logined.php'; ?>
     <h1>明細履歴</h1>
     <div class="container">
-      <?php include VIEW_PATH . 'templates/messages.php'; ?>
-      <?php if(count($details) > 0) { ?>
-         <table class="table table-bordered">
-         <thead class="thead-light">
-        <table>
-            <thead>
+        <?php include VIEW_PATH . 'templates/messages.php'; ?>
+        <?php if (count($details) > 0) { ?>
+            <table class="table table-bordered">
+                <thead class="thead-light">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>注文番号</th>
+                                <th>購入日時</th>
+                                <th>合計金額</th>
+                            </tr>
+                        </thead>
+                    </table>
+                    <?php foreach ($histories as $history) { ?>}
+                    <tr>
+                        <?php print(h($history['oreder_id'])); ?>
+                        <?php print(h($history['date'])); ?>
+                        <?php print(h($history['total'])); ?>
+                        <td>
+                            <form method="post" action="cart.php">
+                                <input type="submit" value="購入明細表示">
+                                <input type="hidden" name="oreder_id" value="<?php print(h($history['oreder_id'])); ?>">
+                            </form>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </table>
+            <table>
+                <thead>
+                    <tr>
+                        <th>商品名</th>
+                        <th>価格</th>
+                        <th>購入数</th>
+                        <th>合計</th>
+                    </tr>
+                </thead>
+            </table>
+            <?php foreach ($details as $detail) { ?>
                 <tr>
-                    <th>商品名</th>
-                    <th>価格</th>
-                    <th>購入数</th>
-                    <th>合計</th>
+                    <?php print(h($detail['name'])); ?>
+                    <?php print(h($detail['price'])); ?>
+                    <?php print(h($detail['amount'])); ?>
+                    <?php print(h($detail['total'])); ?>
                 </tr>
-            </thead>
-       </table> 
-     <?php foreach($details as $detail) { ?>
-        <tr>
-          <?php print(h($detail['name'])); ?>
-          <?php print(h($detail['price'])); ?>
-          <?php print(h($detail['amount'])); ?>
-          <?php print(h($detail['total'])); ?>
-        </tr>
-    <?php } ?>
-     <?php } ?>
-    </body>
+            <?php } ?>
+        <?php } ?>
+</body>
+
 </html>
--- www/view/history_view.php
+++ www/view/history_view.php
@@ -1,43 +1,47 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-<?php include VIEW_PATH. 'templates/head.php'; ?>
-<title>購入履歴</title>
-<link rel="stylesheet" href="<?php print(STYLESHEET_PATH.'cart.css'); ?>">
+  <?php include VIEW_PATH . 'templates/head.php'; ?>
+  <title>購入履歴</title>
+  <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . 'history.css'); ?>">
 </head>
+
 <body>
-  <?php include VIEW_PATH.'templates/cart.php'; ?>
-    <h1>購入履歴</h1>
-    <div class="container">
+  <?php include VIEW_PATH . 'templates/header_logined.php'; ?>
+  <h1>購入履歴</h1>
+  <div class="container">
     <?php include VIEW_PATH . 'templates/messages.php'; ?>
-<?php if(count($histories) > 0) { ?>
-    <table classs="table rable-bodered">
-    <thead claee="thead-light">
- <table>
-  <thead>
-      <tr>
-        <th>注文番号</th>
-        <th>購入日時</th>
-        <th>合計金額</th>
-      </tr>
-  </thead>
- </table>
-    <?php foreach($histories as $history) { ?>}
-    <tr>
-        <?php print(h($history['oreder_id'])); ?>
-        <?php print(h($history['date'])); ?>
-        <?php print(h($history['total'])); ?>
-      <td>
-        <form method="post" action="cart.php"> 
-          <input type="submit" value="購入明細表示">
-          <input type="hidden" name="oreder_id" value="<?php print(h($history['oreder_id'])); ?>">
-        </fprm>
-      </td>
-    </tr>       
+    <?php if (count($histories) > 0) { ?>
+      <table classs="table rable-bodered">
+        <thead claee="thead-light">
+          <table>
+            <thead>
+              <tr>
+                <th>注文番号</th>
+                <th>購入日時</th>
+                <th>合計金額</th>
+              </tr>
+            </thead>
+          </table>
+          <?php foreach ($histories as $history) { ?>}
+          <tr>
+            <?php print(h($history['oreder_id'])); ?>
+            <?php print(h($history['date'])); ?>
+            <?php print(h($history['total'])); ?>
+            <td>
+              <form method="post" action="cart.php">
+                <input type="submit" value="購入明細表示">
+                <input type="hidden" name="oreder_id" value="<?php print(h($history['oreder_id'])); ?>">
+              </form>
+            </td>
+          </tr>
+      </table>
     <?php } ?>
- </body>
-</table>
-<?php } else { ?>
+    
+  <?php } else { ?>
     <p>購入履歴がありません</p>
-<?php } ?>
+  <?php } ?>
+</body>
+
 </html>
