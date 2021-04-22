dd2a50d8a4f9747e971a988c960dc13507841d4e
Takahiro Yoneki <hayabusa186882@gmail.com>
2021-04-23 00:21:52 +0900

    購入履歴画面の実装

---
+++ www/html/history.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../conf/const.php';
+require_once MODEL_PATH . 'functions.php';
+require_once MODEL_PATH . 'user.php';
+require_once MODEL_PATH . 'item.php';
+require_once MODEL_PATH . 'cart.php';
+require_once MODEL_PATH . 'histroies.php';
+
+session_start();
+
+if(is_logined() === false) {
+   redirect_to(LOGIN_URL); 
+}
+
+$db = get_db_connect(); 
+$user = get_login_user($db);
+
+$histories = insert_history($db,$user['user_id']);
+$token = get_csrf_token();
+
+include_once VIEW_PATH . 'history_view.php';
---
+++ www/model/history.php
@@ -0,0 +1,14 @@
+<?php
+require_once MODEL_PATH . 'functions.php';
+require_once MODEL_PATH . 'db.php';
+
+function insert_history($db, $user_id)
+{
+    $sql = "SELECT history.order_id,create_datetime,sum(amount*price) as total
+from history
+inner join details on history.order_id = details.order_id
+group by history.order_id 
+order by create_datetime desc"; //降順(最新版が表示)
+
+    return fetch_all_query($db, $sql, array($user_id));
+}
